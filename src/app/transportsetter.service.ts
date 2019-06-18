4c661cda27645ff6e0c24551da0ee0ae32863fb5
Chris <trolltrader@gmail.com>
2019-06-18 10:52:12 +0100

    Transport Icon Update

--- src/app/transportsetter.service.ts
+++ src/app/transportsetter.service.ts
@@ -53,183 +53,188 @@ export class TransportsetterService {
   SetTransportIcons(unit: Unit){   
     if (this.faction != 'Shaltari'){
       if (unit.transport1Class == 'a'){
-        unit.transport1Icon = 'https://thetrolltrader.com/dzcbuilderbeta/assets/images/transporticons/VehicleOrange.png';       
+        unit.transport1Icon = 'https://www.dropzonecommander.com/builder/assets/images/transporticons/VehicleOrange.png';       
       }
       if (unit.transport1Class == 'A'){
-        unit.transport1Icon = 'https://thetrolltrader.com/dzcbuilderbeta/assets/images/transporticons/VehicleTransportOrange.png';       
+        unit.transport1Icon = 'https://www.dropzonecommander.com/builder/assets/images/transporticons/VehicleTransportOrange.png';       
       }
       if (unit.transport1Class == 'b'){
-        unit.transport1Icon = 'https://thetrolltrader.com/dzcbuilderbeta/assets/images/transporticons/ArtilleryYellow.png';
+        unit.transport1Icon = 'https://www.dropzonecommander.com/builder/assets/images/transporticons/ArtilleryYellow.png';
       }
       if (unit.transport1Class == 'B'){
-        unit.transport1Icon = 'https://thetrolltrader.com/dzcbuilderbeta/assets/images/transporticons/ArtilleryTransportYellow.png';
+        unit.transport1Icon = 'https://www.dropzonecommander.com/builder/assets/images/transporticons/ArtilleryTransportYellow.png';
       }
       if (unit.transport1Class == 'c'){
-        unit.transport1Icon = 'https://thetrolltrader.com/dzcbuilderbeta/assets/images/transporticons/InfantryGreen.png';
+        unit.transport1Icon = 'https://www.dropzonecommander.com/builder/assets/images/transporticons/InfantryGreen.png';
       }
       if (unit.transport1Class == 'C'){
-        unit.transport1Icon = 'https://thetrolltrader.com/dzcbuilderbeta/assets/images/transporticons/InfantryTransportGreen.png';
+        unit.transport1Icon = 'https://www.dropzonecommander.com/builder/assets/images/transporticons/InfantryTransportGreen.png';
       }
       if (unit.transport2Class == 'a'){
-        unit.transport2Icon = 'https://thetrolltrader.com/dzcbuilderbeta/assets/images/transporticons/VehicleOrange.png';       
+        unit.transport2Icon = 'https://www.dropzonecommander.com/builder/assets/images/transporticons/VehicleOrange.png';       
       }
       if (unit.transport2Class == 'A'){
-        unit.transport2Icon = 'https://thetrolltrader.com/dzcbuilderbeta/assets/images/transporticons/VehicleTransportOrange.png';       
+        unit.transport2Icon = 'https://www.dropzonecommander.com/builder/assets/images/transporticons/VehicleTransportOrange.png';       
       }
       if (unit.transport2Class == 'b'){
-        unit.transport2Icon = 'https://thetrolltrader.com/dzcbuilderbeta/assets/images/transporticons/ArtilleryYellow.png';
+        unit.transport2Icon = 'https://www.dropzonecommander.com/builder/assets/images/transporticons/ArtilleryYellow.png';
       }
       if (unit.transport2Class == 'B'){
-        unit.transport2Icon = 'https://thetrolltrader.com/dzcbuilderbeta/assets/images/transporticons/ArtilleryTransportYellow.png';
+        unit.transport2Icon = 'https://www.dropzonecommander.com/builder/assets/images/transporticons/ArtilleryTransportYellow.png';
       }
       if (unit.transport2Class == 'c'){
-        unit.transport2Icon = 'https://thetrolltrader.com/dzcbuilderbeta/assets/images/transporticons/InfantryGreen.png';
+        unit.transport2Icon = 'https://www.dropzonecommander.com/builder/assets/images/transporticons/InfantryGreen.png';
       }
       if (unit.transport2Class == 'C'){
-        unit.transport2Icon = 'https://thetrolltrader.com/dzcbuilderbeta/assets/images/transporticons/InfantryTransportGreen.png';
+        unit.transport2Icon = 'https://www.dropzonecommander.com/builder/assets/images/transporticons/InfantryTransportGreen.png';
       }
       if (unit.transport1Class == 'v'){
-        unit.transport1Icon = 'https://thetrolltrader.com/dzcbuilderbeta/assets/images/transporticons/VampireYellow.png';
+        unit.transport1Icon = 'https://www.dropzonecommander.com/builder/assets/images/transporticons/VampireYellow.png';
       }
       if (unit.transport1Class == 'V'){
-        unit.transport1Icon = 'https://thetrolltrader.com/dzcbuilderbeta/assets/images/transporticons/VampireTransportYellow.png';
+        unit.transport1Icon = 'https://www.dropzonecommander.com/builder/assets/images/transporticons/VampireTransportYellow.png';
       }
       if (unit.transport2Class == 'v'){
-        unit.transport2Icon = 'https://thetrolltrader.com/dzcbuilderbeta/assets/images/transporticons/VampireYellow.png';
+        unit.transport2Icon = 'https://www.dropzonecommander.com/builder/assets/images/transporticons/VampireYellow.png';
       }
       if (unit.transport2Class == 'V'){
-        unit.transport2Icon = 'https://thetrolltrader.com/dzcbuilderbeta/assets/images/transporticons/VampireTransportYellow.png';
+        unit.transport2Icon = 'https://www.dropzonecommander.com/builder/assets/images/transporticons/VampireTransportYellow.png';
       }
     }
     if (this.faction == 'Scourge'){
       if (unit.transport1Class == 'd'){
-        unit.transport1Icon = 'https://thetrolltrader.com/dzcbuilderbeta/assets/images/transporticons/CrabPurple.png';
+        unit.transport1Icon = 'https://www.dropzonecommander.com/builder/assets/images/transporticons/CrabPurple.png';
       }
       if (unit.transport1Class == 'D'){
-        unit.transport1Icon = 'https://thetrolltrader.com/dzcbuilderbeta/assets/images/transporticons/CrabTransportPurple.png';
+        unit.transport1Icon = 'https://www.dropzonecommander.com/builder/assets/images/transporticons/CrabTransportPurple.png';
       }
       if (unit.transport2Class == 'd'){
-        unit.transport2Icon = 'https://thetrolltrader.com/dzcbuilderbeta/assets/images/transporticons/CrabPurple.png';
+        unit.transport2Icon = 'https://www.dropzonecommander.com/builder/assets/images/transporticons/CrabPurple.png';
       }
       if (unit.transport2Class == 'D'){
-        unit.transport2Icon = 'https://thetrolltrader.com/dzcbuilderbeta/assets/images/transporticons/CrabTransportPurple.png';
+        unit.transport2Icon = 'https://www.dropzonecommander.com/builder/assets/images/transporticons/CrabTransportPurple.png';
       }
       if (unit.transport1Class == 's'){
-        unit.transport1Icon = 'https://thetrolltrader.com/dzcbuilderbeta/assets/images/transporticons/ScreamerBlue.png';
+        unit.transport1Icon = 'https://www.dropzonecommander.com/builder/assets/images/transporticons/ScreamerBlue.png';
       }
       if (unit.transport1Class == 'S'){
-        unit.transport1Icon = 'https://thetrolltrader.com/dzcbuilderbeta/assets/images/transporticons/ScreamerTransportBlue.png';
+        unit.transport1Icon = 'https://www.dropzonecommander.com/builder/assets/images/transporticons/ScreamerTransportBlue.png';
       }
       if (unit.transport2Class == 's'){
-        unit.transport2Icon = 'https://thetrolltrader.com/dzcbuilderbeta/assets/images/transporticons/ScreamerBlue.png';
+        unit.transport2Icon = 'https://www.dropzonecommander.com/builder/assets/images/transporticons/ScreamerBlue.png';
       }
       if (unit.transport2Class == 'S'){
-        unit.transport2Icon = 'https://thetrolltrader.com/dzcbuilderbeta/assets/images/transporticons/ScreamerTransportBlue.png';
+        unit.transport2Icon = 'https://www.dropzonecommander.com/builder/assets/images/transporticons/ScreamerTransportBlue.png';
       }
     }
     
     if (this.faction == 'PHR'){
       if (unit.transport1Class == 'm'){
-        unit.transport1Icon = 'https://thetrolltrader.com/dzcbuilderbeta/assets/images/transporticons/MedusaYellow.png';
+        unit.transport1Icon = 'https://www.dropzonecommander.com/builder/assets/images/transporticons/MedusaYellow.png';
       }
       if (unit.transport1Class == 'M'){
-        unit.transport1Icon = 'https://thetrolltrader.com/dzcbuilderbeta/assets/images/transporticons/MedusaTransportYellow.png';
+        unit.transport1Icon = 'https://www.dropzonecommander.com/builder/assets/images/transporticons/MedusaTransportYellow.png';
       }
       if (unit.transport2Class == 'm'){
-        unit.transport2Icon = 'https://thetrolltrader.com/dzcbuilderbeta/assets/images/transporticons/MedusaYellow.png';
+        unit.transport2Icon = 'https://www.dropzonecommander.com/builder/assets/images/transporticons/MedusaYellow.png';
       }
       if (unit.transport2Class == 'M'){
-        unit.transport2Icon = 'https://thetrolltrader.com/dzcbuilderbeta/assets/images/transporticons/MedusaTransportYellow.png';
+        unit.transport2Icon = 'https://www.dropzonecommander.com/builder/assets/images/transporticons/MedusaTransportYellow.png';
       }
       if (unit.transport1Class == 'a'){
-        unit.transport1Icon = 'https://thetrolltrader.com/dzcbuilderbeta/assets/images/transporticons/WalkerOrange.png';
+        unit.transport1Icon = 'https://www.dropzonecommander.com/builder/assets/images/transporticons/WalkerOrange.png';
       }
       if (unit.transport1Class == 'A'){
-        unit.transport1Icon = 'https://thetrolltrader.com/dzcbuilderbeta/assets/images/transporticons/WalkerTransportOrange.png';
+        unit.transport1Icon = 'https://www.dropzonecommander.com/builder/assets/images/transporticons/WalkerTransportOrange.png';
       }
       if (unit.transport2Class == 'a'){
-        unit.transport2Icon = 'https://thetrolltrader.com/dzcbuilderbeta/assets/images/transporticons/WalkerOrange.png';
+        unit.transport2Icon = 'https://www.dropzonecommander.com/builder/assets/images/transporticons/WalkerOrange.png';
       }
       if (unit.transport2Class == 'A'){
-        unit.transport2Icon = 'https://thetrolltrader.com/dzcbuilderbeta/assets/images/transporticons/WalkerTransportOrange.png';
+        unit.transport2Icon = 'https://www.dropzonecommander.com/builder/assets/images/transporticons/WalkerTransportOrange.png';
       }
       if (unit.transport1Class == 'b'){
-        unit.transport1Icon = 'https://thetrolltrader.com/dzcbuilderbeta/assets/images/transporticons/FanBlue.png';
+        unit.transport1Icon = 'https://www.dropzonecommander.com/builder/assets/images/transporticons/FanBlue.png';
       }
       if (unit.transport1Class == 'B'){
-        unit.transport1Icon = 'https://thetrolltrader.com/dzcbuilderbeta/assets/images/transporticons/FanTransportBlue.png';
+        unit.transport1Icon = 'https://www.dropzonecommander.com/builder/assets/images/transporticons/FanTransportBlue.png';
       }
       if (unit.transport2Class == 'b'){
-        unit.transport2Icon = 'https://thetrolltrader.com/dzcbuilderbeta/assets/images/transporticons/FanBlue.png';
+        unit.transport2Icon = 'https://www.dropzonecommander.com/builder/assets/images/transporticons/FanBlue.png';
       }
       if (unit.transport2Class == 'B'){
-        unit.transport2Icon = 'https://thetrolltrader.com/dzcbuilderbeta/assets/images/transporticons/FanTransportBlue.png';
+        unit.transport2Icon = 'https://www.dropzonecommander.com/builder/assets/images/transporticons/FanTransportBlue.png';
       }
     }
     if (this.faction == 'Shaltari'){
       if (unit.transport1Class == 'a'){
-        unit.transport1Icon = 'https://thetrolltrader.com/dzcbuilderbeta/assets/images/transporticons/VehicleOrange.png';
+        unit.transport1Icon = 'https://www.dropzonecommander.com/builder/assets/images/transporticons/VehicleOrange.png';
       }
       if (unit.transport1Class == 'A'){
-        unit.transport1Icon = 'https://thetrolltrader.com/dzcbuilderbeta/assets/images/transporticons/VehicleTransportOrange.png';
+        unit.transport1Icon = 'https://www.dropzonecommander.com/builder/assets/images/transporticons/VehicleTransportOrange.png';
       }
       if (unit.transport2Class == 'a'){
-        unit.transport2Icon = 'https://thetrolltrader.com/dzcbuilderbeta/assets/images/transporticons/VehicleOrange.png';
+        unit.transport2Icon = 'https://www.dropzonecommander.com/builder/assets/images/transporticons/VehicleOrange.png';
       }
       if (unit.transport2Class == 'A'){
-        unit.transport2Icon = 'https://thetrolltrader.com/dzcbuilderbeta/assets/images/transporticons/VehicleTransportOrange.png';
+        unit.transport2Icon = 'https://www.dropzonecommander.com/builder/assets/images/transporticons/VehicleTransportOrange.png';
       }
       if (unit.transport1Class == 'b'){
-        unit.transport1Icon = 'https://thetrolltrader.com/dzcbuilderbeta/assets/images/transporticons/InfantryGreen.png';
+        unit.transport1Icon = 'https://www.dropzonecommander.com/builder/assets/images/transporticons/InfantryGreen.png';
       }
       if (unit.transport1Class == 'B'){
-        unit.transport1Icon = 'https://thetrolltrader.com/dzcbuilderbeta/assets/images/transporticons/InfantryTransportGreen.png';
+        unit.transport1Icon = 'https://www.dropzonecommander.com/builder/assets/images/transporticons/InfantryTransportGreen.png';
       }
       if (unit.transport2Class == 'b'){
-        unit.transport2Icon = 'https://thetrolltrader.com/dzcbuilderbeta/assets/images/transporticons/InfantryGreen.png';
+        unit.transport2Icon = 'https://www.dropzonecommander.com/builder/assets/images/transporticons/InfantryGreen.png';
       }
       if (unit.transport2Class == 'B'){
-        unit.transport2Icon = 'https://thetrolltrader.com/dzcbuilderbeta/assets/images/transporticons/InfantryTransportGreen.png';
+        unit.transport2Icon = 'https://www.dropzonecommander.com/builder/assets/images/transporticons/InfantryTransportGreen.png';
       }
     }
     if (this.faction == 'Resistance'){
       if (unit.transport1Class == 's'){
-        unit.transport1Icon = 'https://thetrolltrader.com/dzcbuilderbeta/assets/images/transporticons/DrillYellow.png';
+        unit.transport1Icon = 'https://www.dropzonecommander.com/builder/assets/images/transporticons/DrillYellow.png';
       }
       if (unit.transport1Class == 'S'){
-        unit.transport1Icon = 'https://thetrolltrader.com/dzcbuilderbeta/assets/images/transporticons/DrillTransportYellow.png';
+        unit.transport1Icon = 'https://www.dropzonecommander.com/builder/assets/images/transporticons/DrillTransportYellow.png';
       }
       if (unit.transport2Class == 's'){
-        unit.transport2Icon = 'https://thetrolltrader.com/dzcbuilderbeta/assets/images/transporticons/DrillYellow.png';
+        unit.transport2Icon = 'https://www.dropzonecommander.com/builder/assets/images/transporticons/DrillYellow.png';
       }
       if (unit.transport2Class == 'S'){
-        unit.transport2Icon = 'https://thetrolltrader.com/dzcbuilderbeta/assets/images/transporticons/DrillTransportYellow.png';
+        unit.transport2Icon = 'https://www.dropzonecommander.com/builder/assets/images/transporticons/DrillTransportYellow.png';
       }
       if (unit.transport1Class == 'e'){
-        unit.transport1Icon = 'https://thetrolltrader.com/dzcbuilderbeta/assets/images/transporticons/WheelsYellow.png';
+        unit.transport1Icon = 'https://www.dropzonecommander.com/builder/assets/images/transporticons/WheelsYellow.png';
       }
       if (unit.transport1Class == 'E'){
-        unit.transport1Icon = 'https://thetrolltrader.com/dzcbuilderbeta/assets/images/transporticons/WheelsTransportYellow.png';
+        unit.transport1Icon = 'https://www.dropzonecommander.com/builder/assets/images/transporticons/WheelsTransportYellow.png';
       }
       if (unit.transport2Class == 'e'){
-        unit.transport2Icon = 'https://thetrolltrader.com/dzcbuilderbeta/assets/images/transporticons/WheelsYellow.png';
+        unit.transport2Icon = 'https://www.dropzonecommander.com/builder/assets/images/transporticons/WheelsYellow.png';
       }
       if (unit.transport2Class == 'E'){
-        unit.transport2Icon = 'https://thetrolltrader.com/dzcbuilderbeta/assets/images/transporticons/WheelsTransportYellow.png';
+        unit.transport2Icon = 'https://www.dropzonecommander.com/builder/assets/images/transporticons/WheelsTransportYellow.png';
       }
       if (unit.transport1Class == 'd'){
-        unit.transport1Icon = 'https://thetrolltrader.com/dzcbuilderbeta/assets/images/transporticons/BusBlue.png';
+        unit.transport1Icon = 'https://www.dropzonecommander.com/builder/assets/images/transporticons/BusBlue.png';
       }
       if (unit.transport1Class == 'D'){
-        unit.transport1Icon = 'https://thetrolltrader.com/dzcbuilderbeta/assets/images/transporticons/BusTransportBlue.png';
+        unit.transport1Icon = 'https://www.dropzonecommander.com/builder/assets/images/transporticons/BusTransportBlue.png';
       }
       if (unit.transport2Class == 'd'){
-        unit.transport2Icon = 'https://thetrolltrader.com/dzcbuilderbeta/assets/images/transporticons/BusBlue.png';
+        unit.transport2Icon = 'https://www.dropzonecommander.com/builder/assets/images/transporticons/BusBlue.png';
       }
       if (unit.transport2Class == 'D'){
-        unit.transport2Icon = 'https://thetrolltrader.com/dzcbuilderbeta/assets/images/transporticons/BusTransportBlue.png';
+        unit.transport2Icon = 'https://www.dropzonecommander.com/builder/assets/images/transporticons/BusTransportBlue.png';
       }
-      
+      if (unit.transport1Class == 'b'){
+        unit.transport1Icon = 'https://www.dropzonecommander.com/builder/assets/images/transporticons/WheelsBlue.png';
+      }
+      if (unit.transport1Class == 'B'){
+        unit.transport1Icon = 'https://www.dropzonecommander.com/builder/assets/images/transporticons/WheelsTransportBlue.png';
+      }
 
     }
   }
